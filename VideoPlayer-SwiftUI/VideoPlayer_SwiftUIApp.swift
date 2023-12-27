9faffb02854fc6e4a6d3454be8d686ae34379b1a
Gaurav Tak <gaurav.tak@roro.io>
2023-12-27 22:38:20 +0530

    Initial Commit

---
+++ VideoPlayer-SwiftUI/VideoPlayer_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  VideoPlayer_SwiftUIApp.swift
+//  VideoPlayer-SwiftUI
+//
+//  Created by Roro Solutions LLP on 27/12/23.
+//
+
+import SwiftUI
+
+@main
+struct VideoPlayer_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
