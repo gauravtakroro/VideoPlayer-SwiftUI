4b3f13984cbd64d3da58f1c70973e3429d5db586
Gaurav Tak <gaurav.tak@roro.io>
2023-12-27 23:43:39 +0530

    Buttons added to launch Video Players Views

--- VideoPlayer-SwiftUI/UIs/HomeUi/HomeView.swift
+++ VideoPlayer-SwiftUI/UIs/HomeUi/HomeView.swift
@@ -9,11 +9,28 @@ import SwiftUI
 
 struct HomeView: View {
     var body: some View {
-        VStack {
+        VStack(spacing: 10) {
             Image(systemName: "video.square.fill")
                 .imageScale(.large)
                 .foregroundColor(.accentColor)
-            Text("Video Player Demo")
+            Text("Video Player Demo").padding(.bottom, 20)
+            Button {
+               print("VideoFile Video Player Tapped")
+            } label: {
+                Text("Launch VideoFile Video Player").bold().underline()
+            }
+            
+            Button {
+                print("VideoUrl Video Player Tapped")
+            } label: {
+                Text("Launch VideoUrl Video Player").bold().underline()
+            }
+            
+            Button {
+                print("YouTubeUrl Video Player Tapped")
+            } label: {
+                Text("Launch YouTubeUrl Video Player").bold().underline()
+            }
         }
         .padding()
     }
